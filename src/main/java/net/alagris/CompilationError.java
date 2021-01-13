7a23883cf88de7da812d217f32e14634933320f6
Alagris <alagris12358@gmail.com>
2021-01-13 14:27:11 +0100

    pos retained in exceptions

--- src/main/java/net/alagris/CompilationError.java
+++ src/main/java/net/alagris/CompilationError.java
@@ -9,7 +9,7 @@ public class CompilationError extends Exception {
 	/**
      *
      */
-    private static final long serialVersionUID = 1L;
+    public static final long serialVersionUID = 1L;
 
     public CompilationError(String msg) {
         super(msg);
@@ -27,10 +27,10 @@ public class CompilationError extends Exception {
         /**
          *
          */
-        private static final long serialVersionUID = 1L;
-        private final Pos firstDeclaration;
-        private final Pos secondDeclaration;
-        private final String name;
+        public static final long serialVersionUID = 1L;
+        public final Pos firstDeclaration;
+        public final Pos secondDeclaration;
+        public final String name;
 
         public DuplicateFunction(Pos firstDeclaration, Pos secondDeclaration,
                                  String name) {
@@ -55,7 +55,7 @@ public class CompilationError extends Exception {
     }
 
     public static class ParseException extends CompilationError {
-        private final Pos node;
+        public final Pos node;
 
         public ParseException(Pos node, Throwable cause) {
             super(cause);
@@ -68,8 +68,8 @@ public class CompilationError extends Exception {
     }
 
     public static class TypecheckException extends CompilationError {
-        private final Pos funcPos, typePos;
-        private final String name;
+        public final Pos funcPos, typePos;
+        public final String name;
 
         public TypecheckException(Pos funcPos, Pos typePos, String name) {
             super("Function "+name+" "+funcPos+" does not conform to type "+typePos);
@@ -80,8 +80,8 @@ public class CompilationError extends Exception {
     }
 
     public static class CompositionTypecheckException extends CompilationError {
-        private final Pos automatonPos;
-        private final Pos typePos;
+        public final Pos automatonPos;
+        public final Pos typePos;
 
         public CompositionTypecheckException(Pos automatonPos,Pos typePos) {
             super("Composition of transduction at "+automatonPos+" violates assertion at "+typePos);
@@ -91,7 +91,7 @@ public class CompilationError extends Exception {
     }
 
     public static class NondeterminismException extends CompilationError {
-        private final Pos nondeterministicStatePos1, nondeterministicStatePos2;
+        public final Pos nondeterministicStatePos1, nondeterministicStatePos2;
 
         public NondeterminismException(Pos nondeterministicStatePos1, Pos nondeterministicStatePos2, String funcName) {
             super("Function "+funcName+" nondeterministically branches to "
@@ -101,12 +101,12 @@ public class CompilationError extends Exception {
             this.funcName = funcName;
         }
 
-        private final String funcName;
+        public final String funcName;
 
     }
 
     public static class KleeneNondeterminismException extends CompilationError {
-        private final Pos kleenePos;
+        public final Pos kleenePos;
 
         public KleeneNondeterminismException(Pos kleenePos) {
             super("Kleene closure on expression that prints output for empty input" +
@@ -118,7 +118,7 @@ public class CompilationError extends Exception {
     }
 
     public static class IllegalCharacter extends CompilationError {
-        private final Pos pos;
+        public final Pos pos;
 
         public IllegalCharacter(Pos pos, String s) {
             super(s);
@@ -128,7 +128,7 @@ public class CompilationError extends Exception {
 
     public static class WeightConflictingToThirdState extends CompilationError {
 
-        private final LexUnicodeSpecification.FunctionalityCounterexampleToThirdState<
+        public final LexUnicodeSpecification.FunctionalityCounterexampleToThirdState<
                 LexUnicodeSpecification.E, LexUnicodeSpecification.P, ?> counterexample;
         public final Pos automatonPos;
 
@@ -144,7 +144,7 @@ public class CompilationError extends Exception {
 
     public static class WeightConflictingFinal extends CompilationError {
 
-        private final LexUnicodeSpecification.FunctionalityCounterexampleFinal<
+        public final LexUnicodeSpecification.FunctionalityCounterexampleFinal<
                 LexUnicodeSpecification.E, LexUnicodeSpecification.P, ?> counterexample;
         public final Pos automatonPos;
 
@@ -170,60 +170,113 @@ public class CompilationError extends Exception {
     }
 
     public static class MissingFunction extends CompilationError {
+        public final Pos pos;
+        public final String id;
+
         public MissingFunction(Pos pos, String id) {
             super("Variable '" + id + "' not found at "+pos);
+            this.pos = pos;
+            this.id = id;
         }
     }
 
     public static class AmbiguousDictionary extends CompilationError {
+        public final IntSeq in;
+        public final IntSeq out1;
+        public final IntSeq out2;
+
         public AmbiguousDictionary(IntSeq in, IntSeq out1, IntSeq out2) {
             super("Input key '"+in.toUnicodeString()+"' has ambiguous outputs '"+out1+"' and '"+out2+"'");
+            this.in = in;
+            this.out1 = out1;
+            this.out2 = out2;
         }
     }
 
     public static class IllegalInformantSize extends CompilationError {
+        public final List<Pair<IntSeq, IntSeq>> text;
+        public final int expectedSize;
+
         public IllegalInformantSize(List<Pair<IntSeq, IntSeq>> text, int expectedSize) {
             super("Expected informant containing "+expectedSize+" element(s) but was "+text.size());
+            this.text = text;
+            this.expectedSize = expectedSize;
         }
     }
 
     public static class IllegalOperandsNumber extends CompilationError {
+        public final List<?> automata;
+        public final int expectedSize;
+
         public IllegalOperandsNumber(List<?> automata, int expectedSize) {
             super("Expected "+expectedSize+" operands but had "+automata.size());
+            this.automata = automata;
+            this.expectedSize = expectedSize;
         }
     }
 
     public static class InvertionNotPossible extends CompilationError {
 
+        public final String msg;
+
         public InvertionNotPossible(String msg) {
             super(msg);
+            this.msg = msg;
         }
     }
 
     public static class DoubleReflectionOnOutput extends CompilationError {
+        public final Pos state;
+        public final LexUnicodeSpecification.E edge;
+
         public DoubleReflectionOnOutput(Pos state, LexUnicodeSpecification.E edge) {
             super("State at "+state+" produces reflection twice. Inversion will lead to too large automata");
+            this.state = state;
+            this.edge = edge;
         }
     }
     public static class RangeWithoutReflection extends CompilationError {
+        public final Pos state;
+        public final LexUnicodeSpecification.E edge;
+
         public RangeWithoutReflection(Pos state, LexUnicodeSpecification.E edge) {
             super("State at "+state+" is a range but its output is not reflected. Inversion leads to nondeterminism");
+            this.state = state;
+            this.edge = edge;
         }
     }
     public static class EpsilonTransitionCycle extends CompilationError {
+        public final Pos state;
+        public final IntSeq output;
+        public final IntSeq output1;
+
         public EpsilonTransitionCycle(Pos state, IntSeq output, IntSeq output1) {
             super("State at "+state+" produces epsilon cycle. Example of cycling outputs "+output+" "+output1);
+            this.state = state;
+            this.output = output;
+            this.output1 = output1;
         }
     }
     public static class AmbiguousAcceptingState extends CompilationError {
 
-        public AmbiguousAcceptingState(Pos sourceState, Pos firstAcceptingState,Pos secondAcceptingState, LexUnicodeSpecification.P firstFinalEdge, LexUnicodeSpecification.P secondFinalEdge) {
+        public final Pos sourceState;
+        public final Pos firstAcceptingState;
+        public final Pos secondAcceptingState;
+        public final LexUnicodeSpecification.P firstFinalEdge;
+        public final LexUnicodeSpecification.P secondFinalEdge;
+
+        public AmbiguousAcceptingState(Pos sourceState, Pos firstAcceptingState, Pos secondAcceptingState, LexUnicodeSpecification.P firstFinalEdge, LexUnicodeSpecification.P secondFinalEdge) {
             super("State at "+sourceState+" leads nondeterministically to another accepting states "+firstAcceptingState+" with output "+firstFinalEdge+" and "+secondAcceptingState+" with output "+secondFinalEdge+" after inversion.");
+            this.sourceState = sourceState;
+            this.firstAcceptingState = firstAcceptingState;
+            this.secondAcceptingState = secondAcceptingState;
+            this.firstFinalEdge = firstFinalEdge;
+            this.secondFinalEdge = secondFinalEdge;
         }
     }
     public static class PromiseReused extends CompilationError {
 
-		private String promise;
+		public String promise;
 
 		public PromiseReused(String promise) {
 			super("Promise identifier "+promise+" was used more than once! Promise ID must be unique across entire project!");
@@ -233,7 +286,7 @@ public class CompilationError extends Exception {
 
 	}
     public static class PromiseNotMade extends CompilationError {
-		private String promise;
+		public String promise;
 		public PromiseNotMade(String promise) {
 			super("Promise identifier "+promise+" was not found!");
 			this.promise = promise;
